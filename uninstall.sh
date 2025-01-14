2606a0a21ecb2d4eeda67e58861163031c8b82fe
Mikhail Medvedev <e-ink-reader@yandex.ru>
2025-01-14 15:11:39 +0300

    Udev rules added

--- uninstall.sh
+++ uninstall.sh
@@ -6,3 +6,4 @@ fi
 rm -rf /usr/bin/Huawei_modem_calculator
 rm -rf /usr/share/applications/Huawei_modem_calculator.desktop
 rm -rf /usr/share/pixmaps/huawei_unlocker64.png
+rm -rf /etc/udev/rules.d//50-myusb.rules
